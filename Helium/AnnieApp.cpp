fc723dbeb06a6e3c137347c6ccd1034837f98d2f
AnnieCat <annieanddoughnut@gmail.com>
2015-07-31 15:16:08 -0700

    did the matrix math to push to a grid in the ino file made it so you could input a color value and flood the screen with it in R, G, and B

--- Helium/AnnieApp.cpp
+++ Helium/AnnieApp.cpp
@@ -2,6 +2,7 @@
 #include <chrono>
 #include <thread>
 #include <iostream>  // For cin, cout, endl
+#include <queue>     // for push on array
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"   //for image
@@ -14,28 +15,82 @@ using namespace std;
 // Application Main
 ///////////////////////////////////////////////////////////////////////////////
 
+
+struct Image
+{
+	std::shared_ptr<uint8_t> pixels;
+	int width, height, comp;
+
+	Image(const char * filename)
+	{
+		auto p = stbi_load(filename, &width, &height, &comp, 0);
+		pixels = std::shared_ptr<uint8_t>(p, stbi_image_free);
+	}
+
+	uint8_t GetPixel(int x, int y, int c) const
+	{
+		return pixels.get()[y*(width*comp) + x*comp + c];
+	}
+};
+
+int myColor = 0;
+
 int main(int argc, char *argv[])
 {
 
+	Image image("rainbowDebug.jpg");
+	int r,g,b;
+
+	
+
 	{
 		MidiPlayer player;
-		int myColor = 1;
+		
+		cout << "red: " << endl;
+		cin >> r;
+		cout << "green: " << endl;
+		cin >> g;
+		cout << "blue: " << endl;
+		cin >> b;
 
-		for (int i = 0; i < 127; i++)
+		player.SendMidiMessage(1, 1, r);
+		player.SendMidiMessage(2, 1, g);
+		player.SendMidiMessage(3, 1, b);
+		std::this_thread::sleep_for(std::chrono::milliseconds(7));
+
+		/*for (int y = 0; y < 4; ++y)
 		{
+			for (int x = 0; x < 32; ++x)
+			{
+				//player.SendMidiMessage(1, x+(y*32), image.GetPixel(x,y,0));
+				player.SendMidiMessage(1, x + (y * 32), image.GetPixel(x, y, 0));
+				std::this_thread::sleep_for(std::chrono::milliseconds(7));
+
+				player.SendMidiMessage(2, x + (y * 32), image.GetPixel(x, y, 1));
+				std::this_thread::sleep_for(std::chrono::milliseconds(7));
+
+				player.SendMidiMessage(3, x + (y * 32), image.GetPixel(x, y, 2));
+				std::this_thread::sleep_for(std::chrono::milliseconds(7));
+			}
+		}*/
+
+		//for (int i = 0; i < 127; i++)
+		//{
+			
 			/*if (myColor < 250)
 				myColor += 50;
 			else
-				myColor = 1;*/
+				myColor = 1;
 
+			uint8_t red = image.GetPixel()
 
 			//minimum color is 1
 			// ( Which grouping of lights, array placement, color)
 			player.SendMidiMessage(1, i, myColor);
 			
 			//7 seems to be minimum
-			std::this_thread::sleep_for(std::chrono::milliseconds(7));
-		}
+			std::this_thread::sleep_for(std::chrono::milliseconds(7));*/
+		//}
 		std::cout << "Done Sending!" << std::endl;
 		std::this_thread::sleep_for(std::chrono::milliseconds(50));
 	}
--- MatrixAnnie/MatrixAnnie.ino
+++ MatrixAnnie/MatrixAnnie.ino
@@ -10,6 +10,7 @@
 #include <midi_Namespace.h>
 #include <midi_Settings.h>
 
+
 MIDI_CREATE_DEFAULT_INSTANCE();
 
 #define PIN 6
@@ -19,33 +20,48 @@ Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(32, 8, PIN,
   NEO_MATRIX_COLUMNS + NEO_MATRIX_ZIGZAG,
   NEO_GRB            + NEO_KHZ800);
 
-int x;
-int y;
+
+int x, y;
+
+
+  
     
 // Note on message will control all LEDS (8 * 16)
 // Channel 0 = 1-127
 // Channel 1 = 128-256
-void HandleNoteOn(byte channel, byte pitch, byte velocity) 
+/*void HandleNoteOn(byte channel, byte pitch, byte velocity) 
 { 
     y = ceil(pitch / 32);
+    if(channel == 1)
+      matrix.drawPixel(pitch - (32 * y), y, matrix.Color(velocity, 0, 0));
+}*/
+int r, g, b;
+void HandleNoteOn(byte channel, byte pitch, byte velocity)
+{
+  if(channel==1)
+    r = velocity;
+  if(channel == 2)
+    g = velocity;
+  if(channel == 3)
+    b = velocity;
+
+    matrix.fillScreen(matrix.Color(r,g,b));
+    matrix.show();
+    r , g, b = 0;
     
-    matrix.drawPixel(pitch - (32*y),y,matrix.Color(velocity, 0, 0));
-    matrix.show();  
 }
+
 
 void setup()
 {
   matrix.begin();
   matrix.setBrightness(32);
+  matrix.setCursor(0,0);
   
   MIDI.begin(MIDI_CHANNEL_OMNI);
   MIDI.setHandleNoteOn(HandleNoteOn); 
-  
+
   delay(250);
-  
-  matrix.fillScreen(0);
-  matrix.setCursor(0,0);
-  
 }
 
 void loop()
